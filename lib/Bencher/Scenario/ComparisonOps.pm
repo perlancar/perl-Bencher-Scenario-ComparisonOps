7bc42b0fb4319ec8f1ada45d47d43bcd7623ae86
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-12-29 13:48:05 +0700

    Release v0.01

--- lib/Bencher/Scenario/ComparisonOps.pm
+++ lib/Bencher/Scenario/ComparisonOps.pm
@@ -11,8 +11,10 @@ our $scenario = {
     summary => 'Benchmark comparison operators',
 
     participants => [
-        {name=>'1k-numeq', code_template=>'my $val =     1; for (1..1000) { if ($val ==     1) {} if ($val ==     2) {} }'},
-        {name=>'1k-streq', code_template=>'my $val = "foo"; for (1..1000) { if ($val eq "foo") {} if ($val eq "bar") {} }'},
+        {name=>'1k-numeq'      , code_template=>'my $val =     1; for (1..1000) { if ($val ==     1) {} if ($val ==     2) {} }'},
+        {name=>'1k-streq-len1' , code_template=>'my $val = "a"  ; for (1..1000) { if ($val eq "a"  ) {} if ($val eq "b"  ) {} }'},
+        {name=>'1k-streq-len3' , code_template=>'my $val = "foo"; for (1..1000) { if ($val eq "foo") {} if ($val eq "bar") {} }'},
+        {name=>'1k-streq-len10', code_template=>'my $val = "abcdefghij"; for (1..1000) { if ($val eq "abcdefghij") {} if ($val eq "klmnopqrst") {} }'},
     ],
 };
 
